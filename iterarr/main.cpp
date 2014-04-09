f842c58d528a4247b0ed8c0d59380f9d6e63a679
kakadu <kakadu.hafanana@gmail.com>
2014-04-09 20:32:15 +0400

    Working....

--- iterarr/main.cpp
+++ iterarr/main.cpp
@@ -1,6 +1,5 @@
 #include <cassert>
 #include <iostream>
-//#include <chrono>
 
 #include <vector>
 #include <sstream>
@@ -20,53 +19,52 @@ uint64_t rdtsc(){
     __asm__ __volatile__ ("rdtsc" : "=a" (lo), "=d" (hi));
     return ((uint64_t)hi << 32) | lo;
 }
+string string_of_mode(Mode m) {
+  switch (m) {
+  case STRAIT: return "Strait";
+  case REVERSE: return "Reverse";
+  case PSEUDO_RANDOM: return "Pseudo";
+  case RANDOM:   return "Random";
+  }
+  return "";
+}
 
+int foo(int x) {
+  return x + 1;
+}
 uint64_t f(int n, Mode m) {
     uint64_t t1, t2;
-    string msg;
+    vector<int> arr(n);
+    vector<int> idx(n);
+    int x;
     if (m == STRAIT) {
         vector<int> arr(n);
-        int x;
-        auto it = arr.begin(), it2 = arr.end();
+	//auto it = arr.begin(), it2 = arr.end();
         t1 = rdtsc();
-        for (; it != arr.end(); ++it) x = (*it);
-        //for (auto& i : arr)     x=i;
+        for (int i=0; i<n; ++i) x = arr[i];
+        //for (; it != it2; ++it) x = (*it);
         t2 = rdtsc();
     } else if (m == REVERSE) {
-        vector<int> arr(n);
-        int x;
-        auto it = arr.rbegin();
         t1 = rdtsc();
-        for (; it != arr.rend(); ++it) x = (*it);
+        for (int i=n-1; i>=0; --i) x = arr[i];
         t2 = rdtsc();
     } else if (m==PSEUDO_RANDOM) {
-        std::default_random_engine generator;
-        std::uniform_int_distribution<int> distribution(0, n);
-        vector<int> idx(n);
-        for (int i=0; i<n; ++i) idx[i] = distribution(generator);
-        int x;
-        vector<int> arr(n);
+        for (int i=0; i<n; ++i) idx[i] = rand()%n;;
 
-        t1 = rdtsc();//start = chrono::steady_clock::now();
+        t1 = rdtsc();
         for (int i=0; i<n; ++i)     x = arr[ idx[i] ];
-        t2 = rdtsc();//end   = chrono::steady_clock::now();
+        t2 = rdtsc();
     } else if (m==RANDOM) {
-        std::random_device rd;
-        std::default_random_engine generator( rd() );
-        std::uniform_int_distribution<int> distribution(0, n);
-        vector<int> idx(n);
-        for (int i=0; i<n; ++i) idx[i] = distribution(generator);
-        int x;
-        vector<int> arr(n);
+        for (int i=0; i<n; ++i) idx[i] = rand() % n;
 
-        t1 = rdtsc();//start = chrono::steady_clock::now();
+        t1 = rdtsc();
         for (int i=0; i<n; ++i)     x = arr[ idx[i] ];
-        t2 = rdtsc();//end   = chrono::steady_clock::now();
+        t2 = rdtsc();
     } else {
         assert(false);
     }
 
-
+    foo(x);
     uint64_t d = (uint64_t)(t2-t1);
     return d;
 }
@@ -86,41 +84,69 @@ int main() {
     long ans;
     cout.precision(10);
     long _32k = 32768;
-    long sizes[] = { _32k, _32k << 1, _32k << 2, _32k << 3, _32k << 4,
-                     _32k << 5,  // 1Mb
-                     _32k << 6,  // 2Mb
-                     _32k << 7,  // 4Mb
-                     _32k << 8,  // 8Mb
-                     _32k << 9,  // 16Mb
-                     _32k << 10, // 32Mb
+    long sizes[] = { _32k, _32k << 1, _32k << 2, _32k << 3, 
+                     _32k * 16,
+                     _32k * 32,   // 1Mb
+                     _32k * 64,   // 2Mb
+                     _32k * 128,  // 4Mb
+                     _32k * 256,  // 8Mb
+                     _32k * 512,  // 16Mb
+                     _32k * 600,  
+                     _32k * 700,  
+                     _32k * 800,  
+                     _32k * 900,  
+                     _32k * 1024, // 32Mb
     };
+    int sizes_count = sizeof(sizes)/sizeof(long);
+    uint64_t results[sizes_count][6];
+	
+    for (int i=0; i<sizes_count; ++i) {
+      results[i][0] = i;
+      results[i][1] = sizes[i];
+      for (int j=2; j<6; j++)
+        results[i][j]=0;
+    }
 
     std::ofstream ofs;
     ofs.open ("cpp.out", std::ofstream::out);
     ofs.precision(10);
-
-    for (int i=0; i< sizeof(sizes)/sizeof(long); ++i) {
+    /*
+    for (int i=0; i<sizes_count; ++i) {
+      for (int j=0; j<6; ++j) {
+        ofs << results[i][j]  << " ";
+      }
+      ofs << endl;
+    } */
+ 
+   for (int m=0; m<=2; ++m) {
+      Mode mode = (Mode)m;
+      for (int i=0; i<sizes_count; ++i) {
         long size = sizes[i];
-        ofs << i << " " << size << " ";
-
-        ans = myavg([size]() { return f(size, STRAIT); });
-        ofs << fixed << ans << " ";
-        cout << "Strait        for " << size << "  takes " << ans << endl;
-
-        ans = myavg([size]() { return f(size, REVERSE); });
-        ofs << fixed << ans << " ";
-        cout << "Reverse       for " << size << "  takes " << ans  << endl;
-
-        ans = myavg([size]() { return f(size, PSEUDO_RANDOM); });
-        ofs << fixed << ans << " ";
-        cout << "Pseudo-random for " << size << "  takes " << ans  << endl;
-
-        ans = myavg([size]() { return f(size, RANDOM); });
-        ofs << fixed << ans << " ";
-        cout << "Really-random for " << size << "  takes " << ans << endl;
-
-        ofs << "\n";
+        cout << string_of_mode(mode) << " for " << size << "  takes ";
+        ans = myavg([mode, size]() { return f(size, mode); });
+	cout << ans << endl;
+        results[i][2+m] = ans;
+      }
+    }
+    
+    Mode mode = RANDOM;
+    
+    for (int i=0; i<sizes_count; ++i) {
+      long size=sizes[i];
+        cout << string_of_mode(mode) << " for " << size << "  takes ";
+        ans = myavg([mode, size]() { return f(size, mode); });
+	cout << ans << endl;
+	results[i][5] = ans;
+    }
+    // printing results;
+    for (int i=0; i<sizes_count; ++i) {
+      for (int j=0; j<6; ++j) {
+ 	uint64_t ans = (j<2) ? results[i][j] : (results[i][j]/1000);
+        ofs << ans << " ";
+      }
+      ofs << endl;
     }
     ofs.close();
+    
     return 0;
 }
